2ff6788689b886d54b1cf4033fafe239dbb32e22
Aaron <mol.aron@hotmail.com>
2023-04-12 01:47:39 +0200

    Project added to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Hopstop from "../images/hopstop.png";
+import Real_Estate_Website from "../images/real-estate-website.png";
 import Aaron_Air from "../images/aaron-air.png";
 import Gym_Website from "../images/gym-website.png";
 import Parallax_Scrolling from "../images/parallax-scrolling.png";
@@ -27,6 +28,13 @@ const Portfolio = ({alternateStyles, activeStyle}) => {
                         <div className="portfolio-item-inner shadow-dark">
                             <div className="portfolio-img">
                                 <a href="https://blade1201.github.io/Hopstop/" target="blank"> <img src={Hopstop} alt="portfolio"/></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="portfolio-item padd-15">
+                        <div className="portfolio-item-inner shadow-dark">
+                            <div className="portfolio-img">
+                                <a href="https://blade1201.github.io/Real_Estate_Website/" target="blank"> <img src={Real_Estate_Website} alt="portfolio"/></a>
                             </div>
                         </div>
                     </div>
